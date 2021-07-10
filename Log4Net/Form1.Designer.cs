4beb445d3bd270c4f7e5bd74bbb5ffc75a2decd4
Marc Milà <milamartosmarc5@gmail.com>
2021-07-10 11:42:53 +0200

    Initial commit

---
+++ Log4Net/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Log4Net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(374, 140);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.Location = new System.Drawing.Point(374, 288);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(75, 23);
+            this.btnSetting.TabIndex = 1;
+            this.btnSetting.Text = "Settings!";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSetting);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Button btnSetting;
+    }
+}
+
---
+++ Log4Net/Form1.cs
@@ -0,0 +1,39 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Log4Net
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Form1));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                logger.Info("this is an info message");
+                logger.Debug("this is a debug message to debug variables");
+                logger.Warn("this is a warn message");
+                logger.Error("this is an error message");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message + " " + ex.StackTrace);
+                //ex.Data
+                //despues se haria el throw
+            }
+        }
+    }
+}
